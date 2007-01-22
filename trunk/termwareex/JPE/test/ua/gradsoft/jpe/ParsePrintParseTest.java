fd586cd4000459ae82eb22219120f7fbd451949d
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2007-01-22 17:19:02 +0000

    1. added new test-case from cotan. 2. Updated Java5PP library (fixed bugs with instance-of priority and >> operator)
    git-svn-id: http://datacenter.gradsoft.ua/repos/termwareex@13314 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- trunk/termwareex/JPE/test/ua/gradsoft/jpe/ParsePrintParseTest.java
+++ trunk/termwareex/JPE/test/ua/gradsoft/jpe/ParsePrintParseTest.java
@@ -73,6 +73,21 @@ public class ParsePrintParseTest extends TestCase
         assertTrue("all files are read",nReadedFiles>0);
     }
     
+    public void testExample4() throws Exception
+    {
+        Configuration configuration = new Configuration();
+        configuration.setJPEHome(".");
+        configuration.setInputDir("testdata/example4");
+        configuration.setOutputDir("output/id-example4");
+        configuration.setTransformationName("ID");
+        configuration.setCreateOutputDir(true);
+        Main main = new Main();
+        main.init(configuration);
+        main.run();
+        int nReadedFiles = checkAllSourcesAreParsed("output/id-example4");
+        assertTrue("all files are read",nReadedFiles>0);        
+    }
+    
     
     private int checkAllSourcesAreParsed(String fname) throws Exception
     {
