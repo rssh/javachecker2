641fb2dc9b9d9651551314df93b56c495ac11e6c
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2007-09-29 10:31:43 +0000

    ant task works
    git-svn-id: http://datacenter.gradsoft.ua/repos/termwareex@13720 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ trunk/termwareex/JPE/src/ua/gradsoft/jpe/CompileTimeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * CompileTimeConstants.java
+ *
+ */
+
+package ua.gradsoft.jpe;
+
+/**
+ *Static constants.
+ * @author rssh
+ */
+public class CompileTimeConstants {
+    
+
+    public static final boolean DEMO = false;
+    
+    public static final int DEMO_MAX_CONFIG_VALUES = 2;
+    
+    public static final boolean DEBUG = false;
+    
+    public static final boolean inIDE = true;
+    
+    
+}
