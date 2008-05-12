c0b9453aed7efd80023039a465303c7c39e277cb
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2008-05-12 20:05:00 +0000

    ivy0fication is finished.
    git-svn-id: http://datacenter.gradsoft.ua/repos/termwareex@13990 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- trunk/termwareex/JavaChecker2/installer/JavaChecker.bat
+++ trunk/termwareex/JavaChecker2/installer/JavaChecker.bat
@@ -1,3 +1,4 @@
 set TermWareHome=$INSTALL_PATH
 set JAVACHECKER_HOME=$INSTALL_PATH
-java -Djavachecker.home="%JAVACHECKER_HOME%" -cp "%TermWareHome%\lib\TermWare2.jar";"%JAVACHECKER_HOME%\lib\JavaChecker2.jar";"%JAVACHECKER_HOME\lib\JavaChecker2Annotations.jar";"%JAVACHECKER_HOME%\lib\TermWareJPP.jar"  ua.gradsoft.javachecker.Main %1 %2 %3 %4 %5 %6 %7 %8 %9     
+java -Djavachecker.home="%JAVACHECKER_HOME%" -cp "%TermWareHome%\lib\TermWare-$termware_version.jar";"%JAVACHECKER_HOME%\lib\JavaChecker-$javachecker_version.jar";"%JAVACHECKER_HOME\lib\JavaChecker2Annotations-$javachecker_version.jar";"%JAVACHECKER_HOME%\lib\TermWareJPP-$termwarejpp_version.jar"  ua.gradsoft.javachecker.Main %1 %2 %3 %4 %5 %6 %7 %8 %9     
+
--- trunk/termwareex/JavaChecker2/installer/JavaChecker.sh
+++ trunk/termwareex/JavaChecker2/installer/JavaChecker.sh
@@ -1,3 +1,3 @@
 TermWareHome="$INSTALL_PATH"
 JAVACHECKER_HOME="$INSTALL_PATH"
-java -Djavachecker.home="$JAVACHECKER_HOME" -cp "$TermWareHome/lib/TermWare2.jar:$JAVACHECKER_HOME/lib/JavaChecker2.jar:$JAVACHECKER_HOME/lib/JavaChecker2Annotations.jar:$JAVACHECKER_HOME/lib/TermWareJPP.jar"  ua.gradsoft.javachecker.Main $*
+java -Djavachecker.home="$JAVACHECKER_HOME" -cp "$TermWareHome/lib/TermWare-$termware_version.jar:$JAVACHECKER_HOME/lib/JavaChecker-$javachecker_version.jar:$JAVACHECKER_HOME/lib/JavaChecker2Annotations-$javachecker_version.jar:$JAVACHECKER_HOME/lib/TermWareJPP-$termwarejpp_version.jar"  ua.gradsoft.javachecker.Main $*
