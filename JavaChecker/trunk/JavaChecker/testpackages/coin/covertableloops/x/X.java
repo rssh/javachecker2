ff2e17722158a209ebd55e5bb0130fc857afa354
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2009-05-02 08:58:21 +0000

    added test for convertable loops
    git-svn-id: http://datacenter.gradsoft.ua/repos/termwareex@15051 e6263924-d73c-0410-abf9-b8ad5fbdd136

---
+++ JavaChecker/trunk/JavaChecker/testpackages/coin/covertableloops/x/X.java
@@ -0,0 +1,16 @@
+package x;
+
+import java.util.List;
+
+public class X
+{
+
+ public static void fun1(List<String> l)
+ {
+   for(int i=0; i<l.size(); ++i) {
+      System.out.println(l.get(i));
+   }
+ }
+
+
+}
